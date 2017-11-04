3b69e6a1716453bc8394a352dee3c5d282e696cb
Or Gaizer <gaizeror@gmail.com>
2017-11-04 03:54:00 +0200

    fixed ex1 and ex2. ex1 - DNS resolv ex2 - fake static DNS

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise1 here
+sudo apt-get -y install curl
+sudo sh -c "grep -q 'nameserver 8.8.8.8' /etc/resolv.conf || echo 'nameserver 8.8.8.8' >> /etc/resolv.conf"
+sudo /etc/init.d/networking restart
